9c5d3f3868ccf25f8d77b891ef1f7f20b88935b6
Aleksey Kharin <email39box@gmail.com>
2023-07-10 14:47:18 +0200

    fix testOneWordSearch, add step

--- src/test/java/com/aptekaEconom/test/SearchTest.java
+++ src/test/java/com/aptekaEconom/test/SearchTest.java
@@ -24,13 +24,19 @@ public class SearchTest extends SearchPage {
   @Test
   @DisplayName("Проверка поиска, в поисковую строку вводится запрос, проверяется поисковая выдача. В выдаче 5 карточек товара.")
   public void testOneWordSearch() {
-    step("В поисковую строку сайта ввожу запрос", () -> {
+    step("Шаг 1. В поисковую строку сайта ввожу запрос", () -> {
       SearchPage.search("Аспирин");
     });
-//    SearchPage.search("Аспирин");
-
-    checkBlocksVisibleOnSearchPage();
-    assertTrue(checkSearchResultTextItemCard("Аспирин"));
-    assertTrue(checkCountItemCardOnPage(5),"Проверяем количество карточек товара на странице выдачи поиска");
+    // SearchPage.search("Аспирин");
+    step("Шаг 2. Проверяю наличие блоков на странице поиска", () -> {
+      checkBlocksVisibleOnSearchPage();
+    });
+    step("Шаг 3. Проверяю наличие текста в результатах поиска", () -> {
+      assertTrue(checkSearchResultTextItemCard("Аспирин"));
+    });
+    step("Шаг 4. Проверяю количество карточек товара на странице выдачи поиска", () -> {
+      assertTrue(checkCountItemCardOnPage(5),
+              "Проверяем количество карточек товара на странице выдачи поиска, должно быть 5");
+    });
   }
 }
