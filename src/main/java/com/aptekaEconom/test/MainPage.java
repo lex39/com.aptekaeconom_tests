a8e545335cb8107249ab3f97db73b95b1dfdab22
Aleksey Kharin <email39box@gmail.com>
2023-07-11 18:01:38 +0200

    finish done ok

--- src/main/java/com/aptekaEconom/test/MainPage.java
+++ src/main/java/com/aptekaEconom/test/MainPage.java
@@ -1,14 +1,16 @@
 package com.aptekaEconom.test;
 
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Cookie;
-
-import static com.codeborne.selenide.Browsers.CHROME;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.refresh;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 
 
@@ -20,6 +22,7 @@ public class MainPage {
 
   @BeforeEach
   public void setUp() {
+    Configuration.browser = "firefox";
     open("https://aptekaeconom.com");
     Selenide.webdriver().driver().getWebDriver().manage().addCookie(new Cookie(nameCookieRegion, valueCookieRegion));
     refresh();
